b0ab2cef7c7d0af6610e7131088699cf30b342d2
shreyamadiga <shreyamadiga@gmail.com>
2022-02-25 05:34:57 +0000

    make it better

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,7 @@ class MoviesController < ApplicationController
 
   def show
     id = params[:id] 
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    @movie = Movie.find(id) 
   end
 
   def index
@@ -16,10 +15,9 @@ class MoviesController < ApplicationController
       session[:filtered_ratings] = @filtered_ratings
     end
 
-    # If the user has specified a sorting mechanism, update session sorting mechanism
+    
     if params[:sorting_mechanism].nil?
-      # If user didn't specify a sorting mechanism, then we're going to sort by the
-      # sorting mechanism in our sessions
+      
     else
       session[:sorting_mechanism] = params[:sorting_mechanism]
     end
@@ -33,12 +31,12 @@ class MoviesController < ApplicationController
 
     @movies = Movie.all
 
-    # Filter movies based on rating if our sessions hash has any ratings in it
+    
     if session[:filtered_ratings]
       @movies = @movies.select{ |movie| session[:filtered_ratings].include? movie.rating }
     end
 
-    # title_sort symbol was placed in the params
+    
     if session[:sorting_mechanism] == "title"
       @movies = @movies.sort! { |a,b| a.title <=> b.title }
       @movie_highlight = "hilite"
@@ -51,11 +49,12 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
+    # default
   end
 
+  
   def create
-    @movie = Movie.create!(params[:movie])
+    @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -63,13 +62,17 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find params[:id]
   end
-
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
+    @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
+
+  
 
   def destroy
     @movie = Movie.find(params[:id])
@@ -78,18 +81,7 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
-  def find_with_same_director
-    @movie = Movie.find(params[:id])
-    # Find director of current movie
-    @current_movie_director = @movie.director
-    if @current_movie_director.nil? or @current_movie_director.empty?
-      # If current movie has no director, flash warning and redirect to movies index page
-      flash[:warning] = "'#{@movie.title}' has no director info."
-      redirect_to movies_path
-    else
-      # Else find all movies with the current movie director
-      @movies_to_render = Movie.find_all_by_director(@current_movie_director)
-    end
-  end
+  
+  
 
 end
